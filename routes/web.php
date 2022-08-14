c103af10df37f8e89e9715d171b9f1c4aa6cd27a
Soeng Souy <soengsouy@gmail.com>
2022-08-15 05:51:11 +0700

    add samplate template

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\ResetPasswordController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::group(['middleware'=>'auth'],function()
+{
+    Route::get('home',function()
+    {
+        return view('home');
+    });
+    Route::get('home',function()
+    {
+        return view('home');
+    });
+});
+
+Auth::routes();
+
+// -----------------------------login-------------------------------//
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'authenticate');
+    Route::get('/logout', 'logout')->name('logout');
+});
+
+// ------------------------------ register ---------------------------------//
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register','storeUser')->name('register');    
+});
+
+// -------------------------- main dashboard ----------------------//
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/home', 'index')->name('home');
+});
+
