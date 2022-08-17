d94242f1ebd0d1d5b0c1f2051fb24a2485d6dcf5
Soeng Souy <soengsouy@gmail.com>
2022-08-17 21:10:19 +0700

    change table null users

--- database/migrations/2014_08_12_000000_create_users_table.php
+++ database/migrations/2014_08_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('user_id');
-            $table->string('email')->unique();
-            $table->string('join_date')->unique();
+            $table->string('name')->nullable();
+            $table->string('user_id')->nullable();
+            $table->string('email')->nullable();
+            $table->string('join_date')->nullable();
             $table->string('phone_number')->nullable();
             $table->string('status')->nullable();
             $table->string('role_name')->nullable();
